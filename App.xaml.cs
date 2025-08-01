a219aabbbea4038227dd434af5b208e107996f76
Noxferus <Samuel.kirk@outlook.com.au>
2025-08-01 14:13:10 +0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MSSS_Satelite_Sensor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
